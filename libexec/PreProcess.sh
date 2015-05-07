7c392d7275e8c0c1ff74d2fd4f6cb752d8f92c59
ifuding <fu_ding1991@qq.com>
2015-05-07 21:19:17 +0800

    updata the shell

--- libexec/PreProcess.sh
+++ libexec/PreProcess.sh
@@ -1,13 +1,13 @@
 # ! /bin/bash
 #
 
-if ./OutLinks.sh
+if ! ./OutLinks.sh
 then
   echo "Failure in generating the OutLinks!"
   exit 1
 fi 
 
-if ./PageRank.sh
+if ! ./PageRank.sh
 then
   echo "Failure in calculating the PageRank!"
   exit 1
